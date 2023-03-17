9ea384826fbfa571b1e28b1452abd1eb9fa236e0
Shivrajchandramohan <shivrajchandramohan@gmail.com>
2023-03-17 23:45:48 +0530

    Welcome

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using CoreProblems;
+
+namespace BasicCorePrograms
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Basic Core Programs");
+
+           
+        }
+    }
+}
